ab69665932fd058a736a2006ca198ad329ebadd3
Tolga Gureli <tolgagureli@outlook.com>
2022-07-03 23:58:39 +0300

    q3

--- atmosware/questions/q3/Main.java
+++ atmosware/questions/q3/Main.java
@@ -1,2 +1,48 @@
-package atmosware.questions.q3;public class Main {
+package atmosware.questions.q3;
+
+import java.util.Scanner;
+
+//
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(palindromeTamamlayiciSayi(scanner.nextLine()));
+    }
+
+    public static String palindromeTamamlayiciSayi(String sayi) {
+
+        try {
+            Integer.parseInt(sayi);
+
+        } catch (NumberFormatException nfe) {
+            return "Format Tamsayı degil";
+        }
+
+        boolean b = true;
+        int count = 0;
+
+        int number = Integer.parseInt(sayi);
+        while (b) {
+            String numString = String.valueOf(number);
+            String numStringRe = reString(numString);
+            if (String.valueOf(number).equals(numStringRe)) {
+                return number - count + " + " + count + " = " + number;
+            } else {
+                number++;
+                count++;
+            }
+        }
+
+        return "";
+    }
+
+    private static String reString(String numString) {
+        StringBuilder reNumber = new StringBuilder();
+        for (int i = numString.length() - 1; i >= 0; i--) {
+            reNumber.append(numString.charAt(i));
+        }
+        return reNumber.toString();
+    }
+
 }
