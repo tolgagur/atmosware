cb327ee1e5ff1fc4dc63f96533696c5a61ed2a3f
Tolga Gureli <tolgagureli@outlook.com>
2022-07-18 00:34:22 +0300

    q4 add

--- atmosware/questions/q4/Main.java
+++ atmosware/questions/q4/Main.java
@@ -1,29 +1,7 @@
 package atmosware.questions.q4;
 
 /*
-Merhaba Osman Bey,
-Öncelikle nasılsınız, umarım tatiliniz iyi geçiyordur :) Gönderdiğiniz algoritma sorusunun çözümünü ekte paylaştım.
-Kısaca çözüm yolunu anlatmaya çalışayım.
-Başlangıçta global index=0 değeri tanımladım. Amacı dizide her kontrol yaptığımda  ulasilacakToplam değeri bulamadığım
-zaman index'i 1 arttırmak ve her defasında diziyi daraltarak ilerlemek. Method başlangıçta dizi uzunluğunu kontrol
-ediyor. Yeterli uzunlukta değil ise false dönüyor. Devamında int sum değişkeni tanımlıyorum. Değeri ise kullanılan
-dizenin index'e göre başlangıç değeri(yani index 2 ise dizide 2. eleman başlangıç değeri olur) olarak tanımlıyorum.
-Dizenin başlangıç değeri ile ulasilacakToplam değer
-eşit olabilir fakat mailde bizden  'dizede yan yana olsun ve toplamları 'ulasilacakToplam' sayısına eşit ' olması
-istendiği için en az iki değer olması gerektiğini düşündüm. Böylelikle for'a girmeden bu kuralı sağlamış oluyorum.
 
-Örneğin alttaki dizeler verilsin, ulasilacakToplam = 1 için;
-arr = {1} -> false
-arr = {1,0} -> true
-arr = {1,2,4,1,0} -> 1,0 -> true
-arr = {1,2,4} -> false
-Bu kural ile ilk değer 1 geldiğinde true dönmesini engelliyorum.
-
-For i, index+1 değeri ile başlar. For en kötü senaryoda dizi uzunluğu kadar döner ve her defasında sum'a dizi
-değerlerini ekler. İçerisinde if ile ulasilacakToplam değeri sum'a eşit olup olmadığını kontrol ediyorum. Eşit ise ilk
-alt kümeyi bulmuş olur ve for'u bitirir true döner. Eşit değil ise toplamaya devam eder. Eğer index dizi uzunluğundan
-küçük ise if'e girer ve index'i 1 arttırır fonksiyonu yeniden çağırır. En kötü senaryoda index ile dizi uzunluğu
-eşitlenir ve dizide böyle bir alt küme olmadığı için false döner.
 
 
 * */
